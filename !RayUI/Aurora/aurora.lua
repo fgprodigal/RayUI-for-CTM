b297766c9550d41fcffbae466aa9ae0f4594b0e4
fgprodigal <fgprodigal@gmail.com>
2011-11-14 12:10:47 +0800

    eventtrace美化..

--- !RayUI/Aurora/aurora.lua
+++ !RayUI/Aurora/aurora.lua
@@ -2832,7 +2832,26 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		ScriptErrorsFrameDialogBG:Hide()
 		R.CreateBD(ScriptErrorsFrame)
 		R.CreateSD(ScriptErrorsFrame)
+		
+		R.CreateBD(EventTraceFrame)
+		R.CreateSD(EventTraceFrame)
+		R.ReskinClose(EventTraceFrameCloseButton)
+		select(1, EventTraceFrameScroll:GetRegions()):Hide()
+		local bu = select(2, EventTraceFrameScroll:GetRegions())
+		bu:SetAlpha(0)
+		bu:Width(17)
 
+		bu.bg = CreateFrame("Frame", nil, EventTraceFrame)
+		bu.bg:Point("TOPLEFT", bu, 0, 0)
+		bu.bg:Point("BOTTOMRIGHT", bu, 0, 0)
+		R.CreateBD(bu.bg, 0)
+
+		local tex = EventTraceFrame:CreateTexture(nil, "BACKGROUND")
+		tex:Point("TOPLEFT", bu.bg)
+		tex:Point("BOTTOMRIGHT", bu.bg)
+		tex:SetTexture(C.Aurora.backdrop)
+		tex:SetGradientAlpha("VERTICAL", 0, 0, 0, .3, .35, .35, .35, .35)
+		
 		FrameStackTooltip:SetScale(UIParent:GetScale())
 		FrameStackTooltip:SetBackdrop(nil)
 
@@ -2847,6 +2866,24 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 		R.Reskin(select(4, ScriptErrorsFrame:GetChildren()))
 		R.Reskin(select(5, ScriptErrorsFrame:GetChildren()))
 		R.Reskin(select(6, ScriptErrorsFrame:GetChildren()))
+		
+		local texs = {
+			"TopLeft",
+			"TopRight",
+			"Top",
+			"BottomLeft",
+			"BottomRight",
+			"Bottom",
+			"Left",
+			"Right",
+			"TitleBG",
+			"DialogBG",
+		}
+		
+		for i=1, #texs do
+			_G["ScriptErrorsFrame"..texs[i]]:SetTexture(nil)
+			_G["EventTraceFrame"..texs[i]]:SetTexture(nil)
+		end
 	elseif addon == "Blizzard_GlyphUI" then
 		GlyphFrameBackground:Hide()
 		GlyphFrameSideInset:DisableDrawLayer("BACKGROUND")
