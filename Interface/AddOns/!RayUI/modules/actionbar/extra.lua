8b7b5d4957a0f992b15bc0088db3af506c9deffd
Ray Wang <fgprodigal@gmail.com>
2012-03-05 11:33:04 +0800

    exbutton初始位置

--- Interface/AddOns/!RayUI/modules/actionbar/extra.lua
+++ Interface/AddOns/!RayUI/modules/actionbar/extra.lua
@@ -1,11 +1,11 @@
 local R, C, L, DB = unpack(select(2, ...))
 
 local holder = CreateFrame('Frame', nil, UIParent)
-holder:Point('TOP', UIParent, 'TOP', 0, -150)
+holder:Point("CENTER", UIParent, "BOTTOM", 500, 500)
 holder:Size(ExtraActionBarFrame:GetSize())
 ExtraActionBarFrame:SetParent(holder)
 ExtraActionBarFrame:ClearAllPoints()
-ExtraActionBarFrame:SetPoint('CENTER', holder, 'CENTER')
+ExtraActionBarFrame:SetPoint("CENTER", holder, "CENTER")
 UIPARENT_MANAGED_FRAME_POSITIONS.ExtraActionBarFrame = nil
 UIPARENT_MANAGED_FRAME_POSITIONS.PlayerPowerBarAlt.extraActionBarFrame = nil
 UIPARENT_MANAGED_FRAME_POSITIONS.CastingBarFrame.extraActionBarFrame = nil
